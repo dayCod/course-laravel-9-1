5b4d41fe731bdbd75d0bfc484c33dbf539ad70b5
dayCod <alayarahman07@gmail.com>
2023-03-11 14:07:15 +0700

    update: nnew login page view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -27,7 +27,7 @@
                 <div class="card-body">
                     <div class="text-center mt-4">
                         <div class="mb-3">
-                            <a href="index.html" class="auth-logo">
+                            <a href="{{ route('register') }}" class="auth-logo">
                                 <img src="{{ asset('backend/assets/images/logo-dark.png') }}" height="30"
                                     class="logo-dark mx-auto" alt="" />
                                 <img src="{{ asset('backend/assets/images/logo-light.png') }}" height="30"
