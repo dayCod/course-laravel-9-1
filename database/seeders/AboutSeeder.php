381e86dfa69bc4d210a1f47a3a258190474fbce1
dayCod <alayarahman07@gmail.com>
2023-04-09 10:10:24 +0700

    update: form about setup

---
+++ database/seeders/AboutSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\About;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class AboutSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        About::create([
+            'title' => fake()->sentence(),
+            'short_title' => fake()->text(30),
+        ]);
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(HomeSliderSeeder::class);
+        $this->call(AboutSeeder::class);
     }
 }
